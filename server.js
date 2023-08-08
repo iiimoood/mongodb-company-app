d8e7494e2b12c643992000734b3a2457054bf268
Dominika Ejsmont <d.ejsmont95@gmail.com>
2023-08-08 12:03:23 +0200

    Add endpoint tests for departments

--- server.js
+++ server.js
@@ -27,10 +27,16 @@ mongoose.connect('mongodb://localhost:27017/companyDB', {
 const db = mongoose.connection;
 
 db.once('open', () => {
-  console.log('Connected to the database');
+  if (process.env.NODE_ENV !== 'test') {
+    console.log('Connected to the database');
+  }
 });
 db.on('error', (err) => console.log('Error ' + err));
 
-app.listen('8000', () => {
-  console.log('Server is running on port: 8000');
+const server = app.listen('8000', () => {
+  if (process.env.NODE_ENV !== 'test') {
+    console.log('Server is running on port: 8000');
+  }
 });
+
+module.exports = server;
